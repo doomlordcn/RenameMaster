86305724a5883081931059e800e256598f395188
DoomLord <yinfei1224@163.com>
2015-12-13 20:20:09 +0800

    创建菜单栏

--- RenameMaster/MainFrm.Designer.cs
+++ RenameMaster/MainFrm.Designer.cs
@@ -28,20 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加文件AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(775, 25);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "Menu";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加文件AToolStripMenuItem,
+            this.退出XToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
+            this.文件ToolStripMenuItem.Text = "文件(&F)";
+            // 
+            // 添加文件AToolStripMenuItem
+            // 
+            this.添加文件AToolStripMenuItem.Name = "添加文件AToolStripMenuItem";
+            this.添加文件AToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.添加文件AToolStripMenuItem.Text = "添加文件(&A)";
+            // 
+            // 退出XToolStripMenuItem
+            // 
+            this.退出XToolStripMenuItem.Name = "退出XToolStripMenuItem";
+            this.退出XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出XToolStripMenuItem.Text = "退出(&X)";
+            // 
+            // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(775, 447);
-            this.Name = "Form1";
+            this.Controls.Add(this.menuStrip);
+            this.Name = "MainFrm";
             this.Text = "RenameMaster";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加文件AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出XToolStripMenuItem;
     }
 }
 
